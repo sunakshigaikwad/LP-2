9e6bd69e89a8ad800160659e2d797c760f645333
Sunakshi Gaikwad <168559841+sunakshigaikwad@users.noreply.github.com>
2025-05-03 10:00:33 +0530

    Update c1.py

--- c1.py
+++ c1.py
@@ -1,1 +1,9 @@
+# 1. console cloud google 
+# 2 create new project
+# 3 Enable the project
+# 4 search admin api go to app engine admin api and click enable
+# 5 go to cloud shell
+# 6 click on code in this file and copy https path only.
+# 7 add git clone https://github.com/sunakshigaikwad/LP-2.git in cloud shell 
+# 8 add command ls in cloud shell then cd LP-2 then ls then python c1.py you can see hello output
 print("Hello")
